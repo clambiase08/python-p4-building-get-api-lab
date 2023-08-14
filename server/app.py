c825586c57a92af39d038dd9f5c7631b5620c11f
Christina Lambiase <christina.lambiase@gmail.com>
2023-08-14 15:45:40 -0700

    complete lab

--- server/app.py
+++ server/app.py
@@ -2,37 +2,61 @@
 
 from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
+from sqlalchemy import desc
 
 from models import db, Bakery, BakedGood
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return '<h1>Bakery GET API</h1>'
+    return "<h1>Bakery GET API</h1>"
 
-@app.route('/bakeries')
+
+@app.route("/bakeries")
 def bakeries():
-    return ''
+    bakeries = Bakery.query.all()
+    bakery_dict = [bakery.to_dict() for bakery in bakeries]
 
-@app.route('/bakeries/<int:id>')
+    response = make_response(jsonify(bakery_dict), 200)
+    response.headers["Content-Type"] = "application/json"
+    return response
+
+
+@app.route("/bakeries/<int:id>")
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter(Bakery.id == id).first()
+    bakery_dict = bakery.to_dict()
+    response = make_response(jsonify(bakery_dict), 200)
+    response.headers["Content-Type"] = "application/json"
+    return response
 
-@app.route('/baked_goods/by_price')
+
+@app.route("/baked_goods/by_price")
 def baked_goods_by_price():
-    return ''
+    baked_goods_by_price = BakedGood.query.order_by(desc(BakedGood.price)).all()
+    baked_goods_by_price_dict = [bg.to_dict() for bg in baked_goods_by_price]
+    response = make_response(jsonify(baked_goods_by_price_dict), 200)
+    response.headers["Content-Type"] = "application/json"
+    return response
 
-@app.route('/baked_goods/most_expensive')
+
+@app.route("/baked_goods/most_expensive")
 def most_expensive_baked_good():
-    return ''
+    most_expensive = BakedGood.query.order_by(desc(BakedGood.price)).first()
+    most_expensive_dict = most_expensive.to_dict()
+    response = make_response(jsonify(most_expensive_dict), 200)
+    response.headers["Content-Type"] = "application/json"
+    return response
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
